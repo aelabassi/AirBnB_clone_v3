101476bcd75647870a342bd7febe818ce0d81095
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-05 13:23:50 +0100

    states view

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """State view """
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import abort, jsonify, request
 from models import storage
 from models.state import State
 
