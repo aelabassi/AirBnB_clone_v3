ee2b1e90478c1db45605a0bac70e9b6fa76769ae
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-05 12:57:41 +0100

    get number of objects in each class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models.amenity import Amenity
+from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models.base_model import BaseModel
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -27,4 +27,5 @@ def stats():
                "reviews": Review,
                "states": State,
                "users": User}
-    return jsonify({key: storage.count(value) for key, value in classes.items()})
+    return jsonify({key: storage.count(value) for key,
+                    value in classes.items()})
