aeb7f1fa454b929502c943878f6ddda7b9b056ac
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-05 12:47:23 +0100

    get number of objects in each class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,24 @@
 """Index module"""
 from api.v1.views import app_views
 from flask import jsonify
+import models
+from models.base_model import BaseModel
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Return status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Return stats"""
+    from models import storage
+    classes = {"amenities": Amenity,
+               "cities": City,
+               "places": Place,
+               "reviews": Review,
+               "states": State,
+               "users": User}
+    return jsonify({key: storage.count(value) for key, value in classes.items()})
