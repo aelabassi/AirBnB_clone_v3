9ed89a52864cdf112ae22474886717540bbffe89
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-05 13:02:34 +0100

    handle 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """App module"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Return a 404 error"""
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
