023e90cbdbee0472790cf91e19615c64d6d7d623
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-05 12:54:41 +0100

    get number of objects in each class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,12 @@
 """Index module"""
 from api.v1.views import app_views
 from flask import jsonify
-import models
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models.base_model import BaseModel
 
 
