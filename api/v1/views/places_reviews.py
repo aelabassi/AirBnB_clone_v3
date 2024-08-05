aeb16cfdb856ab8e39eb461aa8a888585b6f04b9
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-05 19:07:40 +0100

    places and reviews view

---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""Places reviews view """
+from api.v1.views import app_views
+from flask import abort
+from flask import jsonify
+from flask import request
+from models.place import Place
+from models.review import Review
+from models import storage
+
+
+@app_views.route('/places/<place_id>/reviews',
+                 methods=['GET'], strict_slashes=False)
+def get_reviews(place_id):
+    """Return all reviews"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    return jsonify([review.to_dict() for review in place.reviews])
+
+
+@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
+def get_review(review_id):
+    """Return a review"""
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    return jsonify(review.to_dict()), 200
+
+
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_review(review_id):
+    """Delete a review"""
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    storage.delete(review)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
+def post_review(place_id):
+    """Create a review"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'user_id' not in data:
+        abort(400, 'Missing user_id')
+    if 'text' not in data:
+        abort(400, 'Missing text')
+    data['place_id'] = place_id
+    review = Review(**data)
+    storage.new(review)
+    storage.save()
+    return jsonify(review.to_dict()), 201
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_review(review_id):
+    """Update a review"""
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    for key, value in data.items():
+        if key not in ['id', 'user_id', 'place_id',
+                       'created_at', 'updated_at']:
+            setattr(review, key, value)
+    storage.save()
+    return jsonify(review.to_dict()), 200
